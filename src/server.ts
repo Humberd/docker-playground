f6340a8c4a0513283b57053b9796af5504c18d7e
Humberd <sawik11@wp.pl>
2017-08-16 23:54:23 +0200

    New version

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  response.write("Hello from server");
+  response.write("Hello from server!!!");
   response.end();
 });
 
