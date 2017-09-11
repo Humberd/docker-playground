6be7b0b56dfcafac2fa28fdf3cabd49967f30a8e
Maciej Sawicki <humberd2@gmail.com>
2017-09-11 18:00:56 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  response.write("Hello from server!!!");
+  response.write(`${process.env.hostname} - this is a hostname`);
   response.end();
 });
 
