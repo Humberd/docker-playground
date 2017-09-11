730f12be6390412e2824d6d95def8fbbb0c72c38
Maciej Sawicki <humberd2@gmail.com>
2017-09-11 18:10:23 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
+  response.setHeader("content-type", "text/html");
   response.write(`${process.env.hostname} - this is a hostname`);
-  response.write(JSON.stringify(process.env));
+  response.write(`<pre>${JSON.stringify(process.env)}</pre>`);
   response.end();
 });
 
